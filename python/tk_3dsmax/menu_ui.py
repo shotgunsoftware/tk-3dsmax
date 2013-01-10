039591df39162479d17370b46ed48004ee11ebc3
manne ohrstrom <manne@shotgunsoftware.com>
2013-01-10 13:02:36 +0000

    added missing file

---
+++ python/tk_3dsmax/menu_ui.py
@@ -0,0 +1,27 @@
+"""
+Copyright (c) 2012 Shotgun Software, Inc
+----------------------------------------------------
+
+Menu handling for Nuke
+
+"""
+
+import tank
+import sys
+import os
+import unicodedata
+
+from tank.platform.qt import QtCore, QtGui, TankQDialog
+
+class WorkAreaMenu(TankQDialog):
+
+    def __init__(self, parent=None):
+        TankQDialog.__init__(self, parent)
+        # no window border pls
+        self.setWindowFlags(QtCore.Qt.Dialog | QtCore.Qt.FramelessWindowHint)
+        
+    def mousePressEvent(self, event):
+        # if no other widgets accepts it, it means click is outside any button
+        # close dialog
+        self.accept()
+
