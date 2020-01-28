97e3b2289b80cadc801df470ed1f271a0d355710
Jean-François Boismenu <jfboismenu@users.noreply.github.com>
2020-01-28 07:42:05 -0500

    SG-15478: tk-multi-setframerange hook (#14)
    Implements the tk-multi-setframerange hook.

---
+++ hooks/tk-multi-setframerange/basic/frame_operations.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2020 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import pymxs
+
+import sgtk
+
+HookBaseClass = sgtk.get_hook_baseclass()
+
+
+class FrameOperation(HookBaseClass):
+    """
+    Hook called to perform a frame operation with the
+    current scene
+    """
+
+    def get_frame_range(self, **kwargs):
+        """
+        get_frame_range will return a tuple of (in_frame, out_frame)
+
+        :returns: Returns the frame range in the form (in_frame, out_frame)
+        :rtype: tuple[int, int]
+        """
+        current_in = int(pymxs.runtime.animationRange.start.frame)
+        current_out = int(pymxs.runtime.animationRange.end.frame)
+        return (current_in, current_out)
+
+    def set_frame_range(self, in_frame=None, out_frame=None, **kwargs):
+        """
+        set_frame_range will set the frame range using `in_frame` and `out_frame`
+
+        :param int in_frame: in_frame for the current context
+            (e.g. the current shot, current asset etc)
+
+        :param int out_frame: out_frame for the current context
+            (e.g. the current shot, current asset etc)
+
+        """
+        pymxs.runtime.animationRange = pymxs.runtime.interval(in_frame, out_frame)
