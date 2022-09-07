d177be9cf1907978a61eaca3bf8be667a219a046
Aldo Vázquez <108836724+vazquea@users.noreply.github.com>
2022-09-07 16:53:58 -0500

    Fixing dependency error at . (#29)

--- hooks/tk-multi-workfiles2/basic/scene_operation.py
+++ hooks/tk-multi-workfiles2/basic/scene_operation.py
@@ -8,6 +8,8 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+import os
+
 import pymxs
 
 import sgtk
