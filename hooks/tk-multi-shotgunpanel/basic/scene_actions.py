e9204e04c19f2ef8604a5478147c5909d4390b64
Julien Langlois <julien.langlois@autodesk.com>
2025-04-15 17:37:08 -0700

    Fixup forgotten changes

--- hooks/tk-multi-shotgunpanel/basic/scene_actions.py
+++ hooks/tk-multi-shotgunpanel/basic/scene_actions.py
@@ -118,10 +118,7 @@ class MaxActions(HookBaseClass):
             # If this is an Alembic cache, then we can import that.
             if name == "merge" and path.lower().endswith(".abc"):
                 # Note that native Alembic support is only available in Max 2016+.
-                if (
-                    app.engine._max_version_to_year(app.engine._get_max_version())
-                    >= 2016
-                ):
+                if app.engine.max_version_year >= 2016:
                     self._import_alembic(path)
                 else:
                     app.log_warning(
